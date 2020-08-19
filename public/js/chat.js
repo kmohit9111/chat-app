47e0788da1727fb367819b29877e41769cf07c73
Mohit Kumar <kmohit9111@gmail.com>
2020-08-19 21:34:44 +0530

    join,storing users,rendering list

--- public/js/chat.js
+++ public/js/chat.js
@@ -10,23 +10,61 @@ const $messages = document.querySelector('#messages')
 //templates
 const messageTemplate = document.querySelector('#message-template').innerHTML
 const locationMessageTemplate = document.querySelector('#location-message-template').innerHTML
+const sidebarTemplate = document.querySelector('#sidebar-template').innerHTML
+
+const {username,room}=Qs.parse(location.search,{ignoreQueryPrefix:true})
+
+const autoScroll = ()=>{
+    //New message element
+    const $newMessage = $messages.lastElementChild
+
+    //height ofnew message
+    const newMessageStyles=getComputedStyle($newMessage)
+    const newMessageMargin = parseInt(newMessageStyles.marginBottom)
+    const newMessageHeight = $newMessage.offsetHeight + newMessageMargin
+
+    //visible height
+    const visibleHeight = $messages.offsetHeight
+
+    //height of message container
+    const containerHeight = $messages.scrollHeight
+
+    //how far i have scolled
+    const scrollOffset = $messages.scrollTop + visibleHeight
+
+    if(containerHeight - newMessageHeight<=scrollOffset){
+        $messages.scrollTop = $messages.scrollHeight
+    }
+}
 
 socket.on('message',(message)=>{
     console.log(message)
     const html = Mustache.render(messageTemplate,{
+        username:message.username,
         message:message.text,
         createdAt:moment(message.createdAt).format('h:mm a')
     })
     $messages.insertAdjacentHTML('beforeend',html)
+    autoScroll()
 })
 
 socket.on('locationMessage',(message)=>{
     console.log(message)
     const html = Mustache.render(locationMessageTemplate,{
+        username:message.username,
         url:message.url,
         createdAt:moment(message.createdAt).format('h:mm a')
     })
     $messages.insertAdjacentHTML('beforeend',html)
+    autoScroll()
+})
+
+socket.on('roomData',({room,users})=>{
+    const html=Mustache.render(sidebarTemplate,{
+        room,
+        users
+    })
+    document.querySelector('#sidebar').innerHTML = html
 })
 
 $messageForm.addEventListener('submit',(e)=>{
@@ -48,7 +86,7 @@ $messageForm.addEventListener('submit',(e)=>{
 
 $sendLocationButton.addEventListener('click',()=>{
     if(!navigator.geolocation){
-        return alert('Geolocation is not supported bu your browser.')
+        return alert('Geolocation is not supported by your browser.')
     }
     $sendLocationButton.setAttribute('disabled','disabled')
 
@@ -62,4 +100,11 @@ $sendLocationButton.addEventListener('click',()=>{
             console.log('location shared')
         })
     })
+})
+
+socket.emit('join',{username,room},(error)=>{
+    if(error){
+        alert(error)
+        location.href='/'
+    }
 })
